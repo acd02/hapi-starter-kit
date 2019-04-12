577cc04a130a4be4c77164f7721003f795c69491
Alex L <lexa.act@gmail.com>
2019-04-12 17:02:37 +0200

    refactor: clean up code

--- src/handlers/index.ts
+++ src/handlers/index.ts
@@ -1,18 +1,21 @@
 import { Lifecycle } from 'hapi'
-import { head } from 'fp-ts/lib/Array'
 
-import { getQueryStrings, getUrlParam } from '../utils'
-import { QueryStrings, Params } from '../routes'
+import { getQueryString, getUrlParam } from '../utils'
+import { QueryStrings } from '../routes'
 
 export const rootHandler: Lifecycle.Method = (request, reply) => {
-  const queryStringValue = head(getQueryStrings(request, QueryStrings.name)).getOrElse('')
-
-  const paramValue = getUrlParam(request, Params.id).getOrElse('')
+  const maybeQueryString = getQueryString(request, QueryStrings.name)
+  const maybeIdParam = getUrlParam(request.paramsArray)
 
   return reply.view('index', {
-    items: [{ id: 1, label: 'this' }, { id: 2, label: 'is' }, { id: 3, label: 'cool' }],
-    queryStringValue,
-    paramValue
+    title: 'Index',
+    items: [
+      { id: 1, label: 'this' },
+      { id: 2, label: 'is' },
+      { id: 3, label: 'cool' }
+    ],
+    maybeQueryString,
+    maybeIdParam
   })
 }
 
--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,17 +1,19 @@
 import { Request } from 'hapi'
-import { Option } from 'fp-ts/lib/Option'
+import { Option, fromNullable } from 'fp-ts/lib/Option'
 import { head } from 'fp-ts/lib/Array'
 
-export function getQueryStrings(request: Request, queryKey: string): string[] {
-  return Object.entries(request.query)
-    .filter(([key]) => key === queryKey)
-    .map(([_key, value]) => <string>value)
+export function getQueryString(request: Request, queryKey: string): Option<string> {
+  return fromNullable(Object.entries(request.query))
+    .filter(queries => queries.length > 0)
+    .chain(queries =>
+      head(
+        queries
+          .filter(([key]) => key === queryKey)
+          .map(([_key, value]) => (Array.isArray(value) ? value[0] : value))
+      )
+    )
 }
 
-export function getUrlParam(request: Request, paramKey: string): Option<string> {
-  return head(
-    Object.entries(request.params)
-      .filter(([key]) => key === paramKey)
-      .map(([_key, value]) => value)
-  )
+export function getUrlParam(paramsArray: string[]): Option<string> {
+  return head(paramsArray)
 }
